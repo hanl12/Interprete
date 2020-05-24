b2d9b710a621d0c7e33a072c56144b5866e3c36d
hyrre <hyrre9056@gmail.com>
2020-05-23 23:01:45 -0500

    Segundo commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const app = express();
 
+const bodyparser = require('body-parser');
+
 const path = require('path');
 
 app.set('port', 3000);
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(bodyparser.urlencoded({extended: false}));
 
 const peg = require('./parser');
 
-app.use(require('./routes/index'));
+app.use(require('./routes'));
 
 app.listen(app.get('port'), () =>{
     console.log("Servidor cargado en el puerto ", app.get('port'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) =>{
     res.render('index.html');
 });
 
+router.post('/add', (req, res) =>{
+    let texto = req.body.texto;
+    console.log(texto);
+});
+
 module.exports = router;
